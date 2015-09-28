52858861d02bb1e41bf4fa7b70fe57c71a81d7cf
charles <wtx21589@163.com>
2015-09-28 11:58:19 +0800

    get iPhone ip method

---
+++ GetDevIPAddress.h
@@ -0,0 +1,17 @@
+//
+//  GetDevIPAddress.h
+//  ChmtechIOS
+//
+//  Created by taixiangwang on 15/5/29.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetDevIPAddress : NSObject
+
+/*!
+ * get device ip address
+ */
++ (NSString *)deviceIPAdress;
+@end
